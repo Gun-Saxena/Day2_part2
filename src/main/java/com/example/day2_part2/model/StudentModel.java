7bc6ec36e156630dae1dc11518e4b203553b0a33
Gun-Saxena <gunsaxena2004@gmail.com>
2026-01-19 15:10:45 +0530

    Updated the API.

--- src/main/java/com/example/day2_part2/model/StudentModel.java
+++ src/main/java/com/example/day2_part2/model/StudentModel.java
@@ -4,8 +4,10 @@ public class StudentModel {
         private int id;
         private String name;
         private String email;
-        public StudentModel(){
-
+        public StudentModel(int id, String name, String email) {
+            this.id = id;
+            this.name = name;
+            this.email = email;
         }
 
         public int getId() {
